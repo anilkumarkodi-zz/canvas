5425c28070b1694e077df8c84f9b6477302d7d27
Anil Kumar <aniltpl08@gmail.com>
2018-04-05 20:13:19 +0530

    [Anil] | Removed commented test

--- src/CanvasController.java
+++ src/CanvasController.java
@@ -10,28 +10,28 @@ public class CanvasController {
   
   public void draw(String command) throws InValidExpressionException, CanvasCreationException, InValidInputException {
     final CommandValidator commandValidator = new CommandValidator();
-    if (!commandValidator.isValidCommand(command) || !commandValidator.isValidExpression(command)) {
+    if (!commandValidator.isValidCommand(command) ||
+            !commandValidator.isValidExpression(command)) {
       throw new InValidExpressionException();
     }
     
     final String[] split = command.split(" ");
-    final String commandType = split[0];
+    final String userCommand = split[0];
 
-    if (CommandType.C.name().equals(commandType)) {
+    if (CommandType.C.name().equals(userCommand)) {
       canvas = new Canvas(parseInt(split[1]), parseInt(split[2]));
       canvas.create();
     }
-
-    if (CommandType.L.name().equals(commandType)) {
+    if (CommandType.L.name().equals(userCommand)) {
       if (canvas == null) throw new CanvasCreationException("Please draw the canvas before draw line");
       canvas.drawLine(parseInt(split[1]), parseInt(split[2]), parseInt(split[3]), parseInt(split[4]));
     }
     
-    if (CommandType.R.name().equals(commandType)) {
+    if (CommandType.R.name().equals(userCommand)) {
       if (canvas == null) throw new CanvasCreationException("Please draw the canvas before draw rectangle");
       canvas.drawRectangle(parseInt(split[1]), parseInt(split[2]), parseInt(split[3]), parseInt(split[4]));
     }
-    if (CommandType.B.name().equals(commandType)) {
+    if (CommandType.B.name().equals(userCommand)) {
       if (canvas == null) throw new CanvasCreationException("Please draw the canvas before fill the colour");
       canvas.fillColour(parseInt(split[1]), parseInt(split[2]), split[3]+"\t");
     }
--- test/CanvasControllerTest.java
+++ test/CanvasControllerTest.java
@@ -1,36 +1,35 @@
-//import exception.CanvasCreationException;
-//import exception.InValidExpressionException;
-//import exception.InValidInputException;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.powermock.core.classloader.annotations.PrepareForTest;
-//import org.powermock.modules.junit4.PowerMockRunner;
-//
-//
-//import static org.mockito.Mockito.mock;
-//
-//@RunWith(PowerMockRunner.class)
-//@PrepareForTest({Canvas.class})
-//
-//public class CanvasControllerTest {
-//
-//  @Test(expected = InValidExpressionException.class)
-//  public void shouldGetInValidExpressionExceptionWhenInValidInPutGiven() throws InValidExpressionException, CanvasCreationException, InValidInputException {
-//    final CanvasController canvasController = new CanvasController();
-//    canvasController.draw("A 20 20");
-//  }
-//
-//  @Test(expected = CanvasCreationException.class)
-//  public void shouldGetCanvasCreationExceptionWhenDrawLineWithOutCanvas() throws InValidExpressionException, CanvasCreationException, InValidInputException {
-//    final CanvasController canvasController = new CanvasController();
-//    canvasController.draw("L 1 2 6 2");
-//  }
-//
-//  @Test
-//  public void shouldCallCreateCanvasForGivenCommandIsCreateCanvas() throws InValidExpressionException, CanvasCreationException, Exception, InValidInputException {
-//    final Canvas canvasMock = mock(Canvas.class);
-//    final CanvasController canvasController = new CanvasController();
-//    canvasController.draw("C 10 10");
-//  }
-//
-//}
+import exception.CanvasCreationException;
+import exception.InValidExpressionException;
+import exception.InValidInputException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CanvasControllerTest {
+
+  @Test(expected = InValidExpressionException.class)
+  public void shouldGetInValidExpressionExceptionWhenInValidInPutGiven() throws InValidExpressionException, CanvasCreationException, InValidInputException {
+    final CanvasController canvasController = new CanvasController();
+    canvasController.draw("A 20 20");
+  }
+
+  @Test(expected = CanvasCreationException.class)
+  public void shouldGetCanvasCreationExceptionWhenDrawLineWithOutCanvas() throws InValidExpressionException, CanvasCreationException, InValidInputException {
+    final CanvasController canvasController = new CanvasController();
+    canvasController.draw("L 1 2 6 2");
+  }
+
+  @Test
+  public void shouldCallCreateCanvasForGivenCommandIsCreateCanvas() throws InValidExpressionException, CanvasCreationException, Exception, InValidInputException {
+    final Canvas canvasMock = mock(Canvas.class);
+    final String[][] strings = {};
+    when(canvasMock.create()).thenReturn(strings);
+    final CanvasController canvasController = new CanvasController();
+    canvasController.draw("C 10 10");
+    verify(canvasMock.create(), times(1));
+  }
+}
